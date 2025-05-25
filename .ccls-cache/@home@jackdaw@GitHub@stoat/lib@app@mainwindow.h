7299c4f4ba98aa61437e5069d1e7bc7e217cc916
jackdaw114 <jasonsampypvt@gmail.com>
2025-05-25 15:17:32 +0530

    gitignore

--- .ccls-cache/@home@jackdaw@GitHub@stoat/lib@app@mainwindow.h
+++
@@ -1,5 +0,0 @@
-#include "stoatuiexport.h"
-#include <QMainWindow>
-class STOAT_EXPORT mainWindow : public QMainWindow{
-
-};
--- .ccls-cache/@home@jackdaw@GitHub@stoat/lib@app@stapplication.cpp
+++
@@ -1,5 +0,0 @@
-#include "stapplication.h"
-#include "stoatuiexport.h"
-
-StApplication::StApplication(int &argc, char **argv)
-    : QApplication(argc, argv) {}
