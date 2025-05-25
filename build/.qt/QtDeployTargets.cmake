1e08b94377f3da7c40c6e86a65cd496c01bde872
jackdaw114 <80084582+jackdaw114@users.noreply.github.com>
2025-05-25 15:18:29 +0530

    Delete build directory

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,4 +0,0 @@
-set(__QT_DEPLOY_TARGET_StoatPrivate_FILE /home/jackdaw/GitHub/stoat/build/lib/libStoatPrivate.so)
-set(__QT_DEPLOY_TARGET_StoatPrivate_TYPE SHARED_LIBRARY)
-set(__QT_DEPLOY_TARGET_stoat_FILE /home/jackdaw/GitHub/stoat/build/src/stoat)
-set(__QT_DEPLOY_TARGET_stoat_TYPE EXECUTABLE)
